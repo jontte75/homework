d149fb7203e3f19084230c5660f8e6987682e104
jontte75 <joni.korhonen@gmail.com>
2017-01-13 19:16:20 +0200

    FIX unknown image files and urls cause issues
    Added list of known image types, we do not want to download
    anything unknown.

--- getImages.py
+++ getImages.py
@@ -11,13 +11,18 @@ except:
 #some pages have/use security token added to the image url 
 stripSecToken = lambda name: name.split("?")[0]
 
+#some pages img url can point to something "different", so copy just what we think we know
+supImgExtensions = ('.jpg', '.jpeg', '.jpe', '.jif', '.jfif', '.jfi', '.jp2', '.j2k', '.jpf',
+                    '.jpx', '.jpm', '.mj2', '.jxr', '.hdp', '.wdp', '.webp', '.gif', '.png',
+                    '.apng', '.mng', '.tiff', '.tif', '.svg', '.svgz', '.xbm', '.bmp', '.dib',
+                    '.bmp', '.ico')
+
 theUrl = "www.mtv.fi"
 result = requests.get("http://"+theUrl)
 parsedPage = BeautifulSoup(result.content,"lxml")
 imageList = []
 unknownList = []
 for imageTag in parsedPage.findAll("img"):
-    print(imageTag["src"])
     if (imageTag["src"].startswith("http")):
         imageList.append(imageTag["src"])
     elif (imageTag["src"].startswith("//")):
@@ -29,11 +34,25 @@ for imageTag in parsedPage.findAll("img"):
         unknownList.append(imageTag["src"])
 
 for image in imageList:
+    imgName = stripSecToken(image.split('/')[-1])
+    #to be on safe side
+    if (False == imgName.endswith(supImgExtensions) or 5 > len(imgName)):
+        unknownList.append(image)
+        continue
     r = requests.get(image)
     if r.status_code == 200:
-        with open(stripSecToken(image.split('/')[-1]), "wb") as code: 
+        with open(stripSecToken(imgName), "wb") as code: 
             code.write(r.content)
-for string in unknownList:
-    print(string)
-sys.exit(1)
-#print(page)
+    else:
+        unknownList.append(image)
+        imageList.remove(image)
+
+for item in unknownList:
+   if item in imageList:
+       imageList.remove(item)
+
+with open("Output.txt", "w") as text_file:
+    text_file.write("\n".join(imageList))
+
+#for string in unknownList:
+#    print(string)
